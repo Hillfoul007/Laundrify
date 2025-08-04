3bc5c1da32d6a588bc789f0f34a8ac85b5faddbc
Chaman Kataria <115880657+Hillfoul007@users.noreply.github.com>
2025-08-04 12:03:32 +0530

    Update production.js

--- backend/config/production.js
+++ backend/config/production.js
@@ -25,6 +25,7 @@ const config = {
     : [
         // Primary production URLs
         "https://www.laundrify.online",
+        "https://laundrify.online",
         "https://laundrify-app-5su7.onrender.com",
         "https://testversion.onrender.com",
         "https://cleancarepro-1-p2oc.onrender.com",
